d2a135effa64982ad6ccec6e2ddb120334c8a7e8
DYevheniia <deyevheniia@gmail.com>
2023-04-17 19:40:19 +0300

    first commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,27 @@
 const burgerMenu = document.getElementById('burger');
 const headerMenu = document.getElementById('headerMenu');
 
+// let burgers = document.querySelectorAll(".burger-menu")
+// let btmenu = document.querySelector(".btmenu")
+
+// burgers.forEach(function(burger){
+//     burger.onclick = function(){
+//         btmenu.classList.toggle("headerrMenu")
+//     }
+// })
+
 burgerMenu.addEventListener('click', function() {
-    if(headerMenu.style.display === 'none'){
-        headerMenu.style.display ="block"
+    if(headerMenu.style.display === 'block'){
+        headerMenu.style.display ="none"   
+        burgerMenu.classList.toggle("burgeropen")
+        headerMenu.classList.toggle("headerrMenu")
+       
     } else{
-        headerMenu.style.display = "none"
+        headerMenu.style.display = "block"
+        headerMenu.classList.toggle("headerrMenu")
+        burgerMenu.classList.toggle("burgeropen")
     }
 });
+
+
+
